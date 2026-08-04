50c305db74efac592bd01df005be2348b08c31a7
ForexHub <admin@forexhubglobal.com>
2026-08-04 18:58:18 +0800

    Add valid PNG icons

--- vite.config.js
+++ vite.config.js
@@ -16,10 +16,14 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'favicon.svg',
-            sizes: '192x192 512x512',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           }
         ]
       }
